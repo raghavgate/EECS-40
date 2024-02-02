9737ed99061807ac645cba339cf65bb6e484abb3
raghavgate <130261948+raghavgate@users.noreply.github.com>
2024-02-02 12:01:26 -0800

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,101 @@
+package Matrix;
+
+public class Matrix {
+
+    // self-defined attributes
+    // feel free to define your self-defined attributes here
+
+
+    // self-defined utils functions
+    // feel free to define and implement your self-defined utils functions/methods here
+
+
+    // Task 1.1: Load the matrix in
+    public Matrix(int m, int n, String expr) {
+
+    }
+
+    // Task 1.2: print the matrix out
+    public String print() {
+        String res;
+
+        return res;
+    }
+
+    // Task 2.1 Matrix Addition and Subtraction
+    public Matrix add(Matrix other){
+        Matrix res;
+
+        return res;
+    }
+
+    public Matrix sub(Matrix other){
+        Matrix res;
+
+        return res;
+    }
+
+    // Task 2.2: Matrix Multiplication
+    public Matrix mul(Matrix other){
+        Matrix res;
+
+        return res;
+    }
+
+    public Matrix mul(double other){
+        Matrix res;
+
+        return res;
+    }
+
+    // Task 2.3: Matrix Scalar Division
+    public Matrix div(double other){
+        Matrix res;
+
+        return res;
+    }
+
+    // Task 2.4: Matrix Equalization
+    public boolean isEq(Matrix other){
+        boolean isEqual;
+
+        return isEqual;
+    }
+
+    // Task 2.5: Matrix Transpose
+    public Matrix transpose(){
+        Matrix res;
+
+        return res;
+    }
+
+
+    // Task 3: Matrix indexing
+    public Matrix getElements(int row_num) {
+        Matrix res;
+
+        return res;
+    }
+
+    public String getElements(int row_num, int col_num) {
+        String res;
+
+        return res;
+    }
+
+    public void setElements(int row_num, Matrix other) {
+
+    }
+
+    public void setElements(int row_num, int col_num, double other){
+
+    }
+
+    // Task 4: Matrix determinant
+    public String determinant() {
+        String det;
+
+        return det;
+    }
+
+}
